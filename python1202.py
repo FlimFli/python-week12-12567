99e42962659cb6a93d80c5a0488a937b51c6769c
FlimFli <peckbboy2@gmail.com>
2024-08-05 15:22:46 +0700

    first commit

---
+++ python1202.py
@@ -0,0 +1,22 @@
+#2.Assignment Operetor
+a = 10
+
+#2.1 Assing value
+b = a
+print("Assing value:", b)
+
+#2.2 Add and assign value
+b += a #=>: b = b + a
+print("Assing value:", b)
+
+#2.3 Subtract and assign value
+b -= a #=>: b = b + a
+print("Assing value:", b)
+
+#2.4 Multiply and assign value
+b *= a #=>: b = b + a
+print("Assing value:", b)
+
+#2.5 bitwise lishift operator
+b <<= a #=>: b = b + a
+print("Assing value:", b)
---
+++ python1203.py
@@ -0,0 +1,38 @@
+#3.Comparsion Operator
+a = 10
+b = 12
+c = 14
+d = 16
+
+#3.1Output=>: a > b is Fasle
+print('a > b is', a>b)
+
+#3.2 Output=>: a < b is true
+print('a < b is', a<b)
+
+#3.3 Output=>: a == b is False
+print('a == b is', a == b)
+
+#3.4 Output=>: a != b is False
+print('a != b is', a != b)
+
+#3.5 Output=>: a >= b is False
+print('a >= b is', a >= b)
+
+#3.6 Output=>: a <= b is False
+print('a <= b is', a <= b)
+
+#3.7 equal to opearor
+print('c == d is', c == d)
+
+#3.8 not equal to opearor
+print('c != d is', c != d)
+
+#3.9 greater than opearor
+print('c > d is', c > d)
+
+#3.10 less than opearor
+print('c < d is', c < d)
+
+#3.11 greater than or equal to opearor
+print('c >= d is', c >= d)
